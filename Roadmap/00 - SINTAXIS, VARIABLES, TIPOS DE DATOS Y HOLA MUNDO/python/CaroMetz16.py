64058d1e6f5e52239845b206be7b7fa166061b2d
CaroMetz16 <estacion_realidad@hotmail.com>
2025-01-30 20:49:53 -0600

    Create CaroMetz16.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CaroMetz16.py
@@ -0,0 +1,36 @@
+# https://python.org
+
+# Comentario en una linea
+
+"""
+Esto tambien es un 
+comentario en 
+varias lineas
+"""
+
+'''
+Esto tambien es
+un comentario valido
+en varias lineas
+'''
+
+my_variable = "Mi proyecto"
+my_variable = "Mi nuevo proyecto"
+
+# Python no tiene constantes
+
+MY_CONSTANT = "mi constante" # por convencion
+
+my_int = 4
+my_float = 8.5
+my_bool = True
+my_bool = False
+mi_string = "Mi cadena de texto"
+mi_other_string = 'Mi otra cadena de texto'
+
+print ("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
